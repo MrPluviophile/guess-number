a89cf525fe6b8d0434b004d3293942f4fbdd4fc2
MrPluviophile <sisnmi91@gmail.com>
2023-06-06 22:51:04 +0800

    add count

--- guess-number.py
+++ guess-number.py
@@ -1,12 +1,15 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 guess = 0
 while guess != r:
 	guess = input('Please make a guess: ')
+	count += 1
 	guess = int(guess)
 	if guess > r:
 		print('The number is smaller')
 	elif guess < r:
 		print('The number is bigger')
+	print('This is', count, 'guess')
 print('You are correct!')
