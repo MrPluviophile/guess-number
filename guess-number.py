67bbd2ad80d042c26db20e46b221cbc1edb8a023
MrPluviophile <sisnmi91@gmail.com>
2023-06-06 23:06:10 +0800

    add out of range warning

--- guess-number.py
+++ guess-number.py
@@ -1,15 +1,27 @@
 import random
 
-r = random.randint(1, 100)
+while True:
+	start = input('Please input the start of random number: ')
+	start = int(start)
+	end = input('Please input the end of random number: ')
+	end = int(end)
+	if start <= end:
+		break
+	else:
+		print('start should be smaller or equal to end')
+r = random.randint(start, end)
 count = 0
 guess = 0
 while guess != r:
 	guess = input('Please make a guess: ')
 	count += 1
 	guess = int(guess)
-	if guess > r:
-		print('The number is smaller')
-	elif guess < r:
-		print('The number is bigger')
+	if guess <= end and guess >= start:
+		if guess > r:
+			print('The number is smaller')
+		elif guess < r:
+			print('The number is bigger')
+	else:
+		print('The number should within', start, "to", end)
 	print('This is', count, 'guess')
 print('You are correct!')
